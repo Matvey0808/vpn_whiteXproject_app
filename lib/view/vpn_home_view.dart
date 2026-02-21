d1b1e3690538eab0685ceefaa0f371434fbbea4e
Chpek <avto.mat.2000g@gmail.com>
2026-02-21 20:26:05 +0700

    add NavigationBar
    added NavigationBar, views settings and home(vpn)

--- lib/view/vpn_home_view.dart
+++ lib/view/vpn_home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vpn_whitexproject_app/view/vpn_setting_view.dart';
 
 class VpnHomeView extends StatefulWidget {
   const VpnHomeView({super.key});
@@ -8,9 +9,14 @@ class VpnHomeView extends StatefulWidget {
 }
 
 class _VpnHomeViewState extends State<VpnHomeView> {
+  int _currentIndex = 0;
+  final List<Widget> views = [VpnHomeView(), VpnSettingView()];
+
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
     return Scaffold(
+      backgroundColor: Color(0xFFFFFDFA),
       appBar: AppBar(
         toolbarHeight: 90,
         backgroundColor: Color(0xFF161414),
@@ -30,7 +36,7 @@ class _VpnHomeViewState extends State<VpnHomeView> {
                       child: Text(
                         "whiteX.project",
                         style: TextStyle(
-                          fontSize: isBreakpointWidth ? 20 : 16,
+                          fontSize: isBreakpointWidth ? 22 : 18,
                           color: Colors.white,
                           fontWeight: FontWeight.w400,
                         ),
@@ -41,6 +47,43 @@ class _VpnHomeViewState extends State<VpnHomeView> {
               ],
             );
           },
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: EdgeInsets.only(bottom: width * 0.1),
+        child: SafeArea(
+          child: Container(
+            height: 76,
+            margin: EdgeInsets.symmetric(horizontal: width * 0.25),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(width: 1.5, color: Colors.black12),
+              borderRadius: BorderRadius.circular(32),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    setState(() => _currentIndex = 0);
+                  },
+                  child: Image.asset(
+                    "assets/IconSettings.png",
+                    color: _currentIndex == 0 ? Colors.blue : Colors.black,
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    setState(() => _currentIndex = 1);
+                  },
+                  child: Image.asset(
+                    "assets/iconVpn.png",
+                    color: _currentIndex == 1 ? Colors.blue : Colors.black,
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
---
+++ lib/view/vpn_setting_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class VpnSettingView extends StatelessWidget {
+  const VpnSettingView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("pisya"),
+    );
+  }
+}
