8b88c94e3b6bd46a8dae52358c5532223f8069eb
Matvey0808 <avto.mat.2000g@gmail.com>
2026-03-07 15:14:26 +0700

    add MethodChannel in MainActivity

--- android/app/src/main/kotlin/com/example/vpn_whitexproject_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/vpn_whitexproject_app/MainActivity.kt
@@ -1,5 +1,33 @@
 package com.example.vpn_whitexproject_app
 
 import io.flutter.embedding.android.FlutterActivity
+import android.app.Application
+import android.content.Intent
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import kotlinx.coroutines.channels.Channel
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    private val CHANNEL = "vpn"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL)
+            .setMethodCallHandler { call, result ->
+                when(call.method) {
+                    "startService" -> {
+                        startService(Intent(this, MyService::class.java))
+                        result.success("Service started")
+                    }
+
+                    "stopService" -> {
+                        stopService(Intent(this, MyService::class.java))
+                        result.success(("Service stopped"))
+                    }
+
+                    else -> result.notImplemented()
+                }
+            }
+    }
+}
