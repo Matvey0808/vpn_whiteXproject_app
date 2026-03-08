61e9c914479c7877582b65d2ffd33f88fb74b656
Matvey0808 <avto.mat.2000g@gmail.com>
2026-03-08 15:27:01 +0700

    add foreground service

--- android/app/src/main/kotlin/com/example/vpn_whitexproject_app/MyService.kt
+++ android/app/src/main/kotlin/com/example/vpn_whitexproject_app/MyService.kt
@@ -1,19 +1,38 @@
 package com.example.vpn_whitexproject_app
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
 import android.content.Intent
+import android.os.Build
 import android.os.IBinder
 import android.util.Log
+import androidx.core.app.NotificationCompat
 
 class MyService : Service() {
 
     override fun onCreate() {
         super.onCreate()
         Log.d("MyService", "OnCreate called")
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel("channel_vpn", "VPN Service Channel",
+                NotificationManager.IMPORTANCE_DEFAULT)
+            val manager = getSystemService(NotificationManager::class.java)
+            manager.createNotificationChannel(channel)
+        }
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        Log.d("MyService", "onStartCommand called")
+        Log.d("MyService", "onStart called")
+        val notification = NotificationCompat.Builder(this, "channel_vpn")
+            .setContentTitle("VPN running")
+            .setContentText("is active")
+            .setSmallIcon(android.R.drawable.ic_lock_lock)
+            .setOngoing(true)
+            .build()
+
+        startForeground(1, notification)
         return START_STICKY
     }
 
