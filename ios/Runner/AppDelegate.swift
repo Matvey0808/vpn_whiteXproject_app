d3f944297746607dba1f8970a63410280e51bcd7
Ilya Nixan <thenixan@gmail.com>
2026-03-22 17:47:11 +0300

    Add CocoaPods integration and iOS VPN channel
    Add Podfile.lock and update Xcode project/workspace to include
    CocoaPods support (Pods group, xcconfigs, frameworks, resource copy
    and manifest check build phases).
    
    Make AppDelegate conform to FlutterImplicitEngineDelegate,
    register plugins with the implicit engine and expose a "vpn"
    FlutterMethodChannel handling startService/stopService.
    
    Update Info.plist to include UIScene configuration and input
    event keys. Remove MinimumOSVersion from AppFrameworkInfo.plist.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,12 +2,36 @@ import Flutter
 import UIKit
 
 @main
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
+  private var vpnChannel: FlutterMethodChannel?
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
+    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
+
+    let messenger = engineBridge.applicationRegistrar.messenger()
+    vpnChannel = FlutterMethodChannel(
+      name: "vpn",
+      binaryMessenger: messenger
+    )
+
+    vpnChannel?.setMethodCallHandler { (call, result) in
+      switch call.method {
+      case "startService":
+        NSLog("VPN startService called on iOS")
+        result("Service started")
+      case "stopService":
+        NSLog("VPN stopService called on iOS")
+        result("Service stopped")
+      default:
+        result(FlutterMethodNotImplemented)
+      }
+    }
   }
 }
